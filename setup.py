d1b632ecb341dcc2464bb08cd8fb844e5cb36d90
Your Name <example@example.com>
2025-05-18 14:34:03 +0800

    add adspower

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="bitnet-api",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages() + ['adspower_api'],
     install_requires=[
         "requests>=2.25.0",
     ],
@@ -19,4 +19,4 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-) 
+)
